32fddc0021232b818c89d22e6d68e128a97dd81f
James Talmage <james@talmage.io>
2015-11-19 17:28:30 -0500

    add some sync tests

---
+++ test3.js
@@ -0,0 +1,19 @@
+var tap = require('tap');
+
+tap.test('test a3', function (t) {
+  t.plan(1);
+  t.pass();
+  t.end();
+});
+
+tap.test('test b3', function (t) {
+  t.plan(1);
+  t.pass();
+  t.end();
+});
+
+tap.test('test c3', function (t) {
+  t.plan(1);
+  t.pass();
+  t.end();
+});
